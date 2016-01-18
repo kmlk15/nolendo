e9776b092508d34b3788f6f7f425760f11ca11c1
Keith Lui <kmlk15@gmail.com>
2016-01-17 19:00:20 -0800

    download file

--- src/main/scala/OnePiece.scala
+++ src/main/scala/OnePiece.scala
@@ -2,6 +2,8 @@ import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClients
 import java.io.StringWriter
 import org.apache.commons.io.IOUtils
+import java.io.File
+import java.io.FileOutputStream
 
 object OnePiece {
 
@@ -10,7 +12,8 @@ object OnePiece {
     val id = args(0)
     
     val httpclient = HttpClients.createDefault()
-    val httpget = new HttpGet("http://dmdata.leyingtuan.com/mobileapp/ckplayer65/video.php?url=" + id + "_tudou")
+    //val httpget = new HttpGet("http://dmdata.leyingtuan.com/mobileapp/ckplayer65/video.php?url=" + id + "_tudou")
+    val httpget = new HttpGet("http://api.ktkkt.com/tyyp.php?v=" + id + "&t=ck")
     val response = httpclient.execute(httpget)
 
     try {
@@ -21,13 +24,25 @@ object OnePiece {
 
       (xml \ "video").foreach(video => {
         val f = (video \ "file").text
-        println(f)
+
+        val fileGet = new HttpGet(f);
+        val fileResponse = httpclient.execute(fileGet)
+        try {
+          val fileEntity = fileResponse.getEntity()
+          if (fileEntity != null) {
+            val file = new File(id + ".flv")
+            val outputStream = new FileOutputStream(file)
+            val inputStream = fileEntity.getContent()
+            IOUtils.copy(inputStream, outputStream)
+            outputStream.close
+          }
+        } finally {
+          fileResponse.close
+        }
       })
 
     } finally {
       response.close()
     }
-
   }
-
 }
