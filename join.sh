6321f3c84f24b2a14679636320971647f9e8ae9e
Keith Lui <kmlk15@gmail.com>
2016-10-15 17:19:41 -0700

    update

---
+++ join.sh
@@ -0,0 +1,156 @@
+./ffmpeg -i 498-499-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 498-499-0.ts
+sleep 1
+./ffmpeg -i 498-499-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 498-499-1.ts
+sleep 1
+./ffmpeg -i 498-499-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 498-499-2.ts
+sleep 1
+./ffmpeg -i 498-499-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 498-499-3.ts
+sleep 1
+./ffmpeg -i 498-499-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 498-499-4.ts
+sleep 1
+./ffmpeg -i 498-499-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 498-499-5.ts
+sleep 1
+./ffmpeg -i 498-499-6.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 498-499-6.ts
+sleep 1
+./ffmpeg -i 'concat:498-499-0.ts|498-499-1.ts|498-499-2.ts|498-499-3.ts|498-499-4.ts|498-499-5.ts|498-499-6.ts' -c copy -bsf:a aac_adtstoasc 498-499.mp4
+sleep 1
+./ffmpeg -i 507-510-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-0.ts
+sleep 1
+./ffmpeg -i 507-510-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-1.ts
+sleep 1
+./ffmpeg -i 507-510-10.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-10.ts
+sleep 1
+./ffmpeg -i 507-510-11.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-11.ts
+sleep 1
+./ffmpeg -i 507-510-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-2.ts
+sleep 1
+./ffmpeg -i 507-510-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-3.ts
+sleep 1
+./ffmpeg -i 507-510-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-4.ts
+sleep 1
+./ffmpeg -i 507-510-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-5.ts
+sleep 1
+./ffmpeg -i 507-510-6.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-6.ts
+sleep 1
+./ffmpeg -i 507-510-7.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-7.ts
+sleep 1
+./ffmpeg -i 507-510-8.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-8.ts
+sleep 1
+./ffmpeg -i 507-510-9.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 507-510-9.ts
+sleep 1
+./ffmpeg -i 'concat:507-510-0.ts|507-510-1.ts|507-510-2.ts|507-510-3.ts|507-510-4.ts|507-510-5.ts|507-510-6.ts|507-510-7.ts|507-510-8.ts|507-510-9.ts|507-510-10.ts|507-510-11.ts' -c copy -bsf:a aac_adtstoasc 507-510.mp4
+sleep 1
+./ffmpeg -i 511-513-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-0.ts
+sleep 1
+./ffmpeg -i 511-513-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-1.ts
+sleep 1
+./ffmpeg -i 511-513-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-2.ts
+sleep 1
+./ffmpeg -i 511-513-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-3.ts
+sleep 1
+./ffmpeg -i 511-513-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-4.ts
+sleep 1
+./ffmpeg -i 511-513-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-5.ts
+sleep 1
+./ffmpeg -i 511-513-6.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-6.ts
+sleep 1
+./ffmpeg -i 511-513-7.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 511-513-7.ts
+sleep 1
+./ffmpeg -i 'concat:511-513-0.ts|511-513-1.ts|511-513-2.ts|511-513-3.ts|511-513-4.ts|511-513-5.ts|511-513-6.ts|511-513-7.ts' -c copy -bsf:a aac_adtstoasc 511-513.mp4
+sleep 1
+./ffmpeg -i 514-516-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-0.ts
+sleep 1
+./ffmpeg -i 514-516-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-1.ts
+sleep 1
+./ffmpeg -i 514-516-10.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-10.ts
+sleep 1
+./ffmpeg -i 514-516-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-2.ts
+sleep 1
+./ffmpeg -i 514-516-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-3.ts
+sleep 1
+./ffmpeg -i 514-516-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-4.ts
+sleep 1
+./ffmpeg -i 514-516-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-5.ts
+sleep 1
+./ffmpeg -i 514-516-6.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-6.ts
+sleep 1
+./ffmpeg -i 514-516-7.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-7.ts
+sleep 1
+./ffmpeg -i 514-516-8.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-8.ts
+sleep 1
+./ffmpeg -i 514-516-9.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 514-516-9.ts
+sleep 1
+./ffmpeg -i 'concat:514-516-0.ts|514-516-1.ts|514-516-2.ts|514-516-3.ts|514-516-4.ts|514-516-5.ts|514-516-6.ts|514-516-7.ts|514-516-8.ts|514-516-9.ts|514-516-10.ts' -c copy -bsf:a aac_adtstoasc 514-516.mp4
+sleep 1
+./ffmpeg -i 505-506-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 505-506-0.ts
+sleep 1
+./ffmpeg -i 505-506-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 505-506-1.ts
+sleep 1
+./ffmpeg -i 505-506-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 505-506-2.ts
+sleep 1
+./ffmpeg -i 505-506-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 505-506-3.ts
+sleep 1
+./ffmpeg -i 505-506-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 505-506-4.ts
+sleep 1
+./ffmpeg -i 505-506-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 505-506-5.ts
+sleep 1
+./ffmpeg -i 'concat:505-506-0.ts|505-506-1.ts|505-506-2.ts|505-506-3.ts|505-506-4.ts|505-506-5.ts' -c copy -bsf:a aac_adtstoasc 505-506.mp4
+sleep 1
+./ffmpeg -i 503-504-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 503-504-0.ts
+sleep 1
+./ffmpeg -i 503-504-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 503-504-1.ts
+sleep 1
+./ffmpeg -i 503-504-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 503-504-2.ts
+sleep 1
+./ffmpeg -i 503-504-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 503-504-3.ts
+sleep 1
+./ffmpeg -i 503-504-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 503-504-4.ts
+sleep 1
+./ffmpeg -i 503-504-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 503-504-5.ts
+sleep 1
+./ffmpeg -i 'concat:503-504-0.ts|503-504-1.ts|503-504-2.ts|503-504-3.ts|503-504-4.ts|503-504-5.ts' -c copy -bsf:a aac_adtstoasc 503-504.mp4
+sleep 1
+./ffmpeg -i 500-502-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-0.ts
+sleep 1
+./ffmpeg -i 500-502-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-1.ts
+sleep 1
+./ffmpeg -i 500-502-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-2.ts
+sleep 1
+./ffmpeg -i 500-502-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-3.ts
+sleep 1
+./ffmpeg -i 500-502-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-4.ts
+sleep 1
+./ffmpeg -i 500-502-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-5.ts
+sleep 1
+./ffmpeg -i 500-502-6.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-6.ts
+sleep 1
+./ffmpeg -i 500-502-7.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-7.ts
+sleep 1
+./ffmpeg -i 500-502-8.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-8.ts
+sleep 1
+./ffmpeg -i 500-502-9.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 500-502-9.ts
+sleep 1
+./ffmpeg -i 'concat:500-502-0.ts|500-502-1.ts|500-502-2.ts|500-502-3.ts|500-502-4.ts|500-502-5.ts|500-502-6.ts|500-502-7.ts|500-502-8.ts|500-502-9.ts' -c copy -bsf:a aac_adtstoasc 500-502.mp4
+sleep 1
+./ffmpeg -i 494-497-0.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-0.ts
+sleep 1
+./ffmpeg -i 494-497-1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-1.ts
+sleep 1
+./ffmpeg -i 494-497-2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-2.ts
+sleep 1
+./ffmpeg -i 494-497-3.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-3.ts
+sleep 1
+./ffmpeg -i 494-497-4.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-4.ts
+sleep 1
+./ffmpeg -i 494-497-5.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-5.ts
+sleep 1
+./ffmpeg -i 494-497-6.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-6.ts
+sleep 1
+./ffmpeg -i 494-497-7.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-7.ts
+sleep 1
+./ffmpeg -i 494-497-8.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-8.ts
+sleep 1
+./ffmpeg -i 494-497-9.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts 494-497-9.ts
+sleep 1
+./ffmpeg -i 'concat:494-497-0.ts|494-497-1.ts|494-497-2.ts|494-497-3.ts|494-497-4.ts|494-497-5.ts|494-497-6.ts|494-497-7.ts|494-497-8.ts|494-497-9.ts' -c copy -bsf:a aac_adtstoasc 494-497.mp4
+sleep 1
---
+++ tools/src/main/scala/Joiner.scala
@@ -0,0 +1,26 @@
+import java.io.File
+
+object Joiner {
+
+  def main(args: Array[String]) {
+	val d = new File(args(0))  
+    
+    val list = d.listFiles.filter(_.isFile).map(_.getName()).filter(_.contains(".mp4")).toList
+
+    list.groupBy(_.substring(0, 7)).foreach { case (k, v) => {
+      val group = v.map(name => {
+        val prefix = name.split('.')(0)
+        println(s"./ffmpeg -i $name -c copy -bsf:v h264_mp4toannexb -f mpegts $prefix.ts")
+        println("sleep 1")
+        
+        prefix
+      })
+      val sorted = group.sortWith((a: String, b: String) => a.split('-')(2).toInt < b.split('-')(2).toInt).map(n => s"$n.ts")
+      
+      println(s"./ffmpeg -i 'concat:${sorted.mkString("|")}' -c copy -bsf:a aac_adtstoasc $k.mp4")
+      println("sleep 1")
+      println(s"rm -rf ${sorted.mkString(" ")}")
+    }}
+  }
+  
+}
